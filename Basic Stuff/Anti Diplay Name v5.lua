960cea3049f750ce063a9631cd3aa6b606bd9349
AreexYT <88655156+AreexYT@users.noreply.github.com>
2022-03-19 13:15:26 -0700

    Update Anti Diplay Name v5.lua

--- Basic Stuff/Anti Diplay Name v5.lua
+++ Basic Stuff/Anti Diplay Name v5.lua
@@ -4,24 +4,25 @@ Anti Diplay Name v5.lua
 
 ]]--
 
-repeat wait() until game:IsLoaded()
+local AntiDisplayName = loadstring(game:HttpGet('https://pastebin.com/raw/hCMXnKWa'))(function()
+    getgenv().Preferences = {}
+end)
 
-while true do
+Preferences = {
+    RetroNaming = false,
+    ShowOriginalName = true,
+    ApplyToLeaderboard = true,
+    IdentifyFriends = {Toggle = true, Identifier = '[Friend]'},
+    IdentifyBlocked = {Toggle = true, Identifier = '[Blocked]'},
+    IdentifyPremium = {Toggle = true, Identifier = '[Premium]'},
+    IdentifyDeveloper = {Toggle = true, Identifier = '[Game Dev]'},
+    SpoofLocalPlayer = {Toggle = false, UseRandomName = true, NewName = 'Random Name Lol'},
+    Orientation = 'Horizontal'
+}
 
-	local AntiDisplayName = loadstring(game:HttpGet('https://pastebin.com/raw/hCMXnKWa'))(function()
-	getgenv().Preferences = {}
-	end)
-
-	Preferences = {
-	RetroNaming = true,
-	ShowOriginalName = true,
-	ApplyToLeaderboard = true,
-	IdentifyFriends = {Toggle = true, Identifier = '[Friend]'},
-	IdentifyBlocked = {Toggle = true, Identifier = '[Blocked]'},
-	IdentifyPremium = {Toggle = true, Identifier = '[Premium]'},
-	IdentifyDeveloper = {Toggle = true, Identifier = '[Game Dev]'},
-	SpoofLocalPlayer = {Toggle = true, UseRandomName = true, NewName = 'Random Name Lol'},
-	Orientation = 'Horizontal'}
-	
-	wait()
-end
+--[[
+    -- Terms of Service
+     - Credit me at all times, please: mothra#4150 or https://v3rmillion.net/member.php?action...e&uid=2988.
+     - Don't sell this script.
+     - Don't steal credit for this script.
+]]
