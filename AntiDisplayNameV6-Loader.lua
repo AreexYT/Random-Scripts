71067c45868f50858daaa656e70fb30e625e3abb
AreexYT <88655156+AreexYT@users.noreply.github.com>
2022-04-06 12:47:01 -0700

    Create AntiDisplayNameV6-Loader.lua

---
+++ AntiDisplayNameV6-Loader.lua
@@ -0,0 +1,23 @@
+--[[
+
+Anti Diplay Name v5.lua
+
+]]--
+while true do
+local AntiDisplayName = loadstring(game:HttpGet('https://raw.githubusercontent.com/AreexYT/Random-Scripts/main/AntiDisplayNameV6.lua'))(function()
+    getgenv().Preferences = {}
+end)
+
+Preferences = {
+    RetroNaming = false,
+    ShowOriginalName = true,
+    ApplyToLeaderboard = true,
+    IdentifyFriends = {Toggle = true, Identifier = '[Friend]'},
+    IdentifyBlocked = {Toggle = true, Identifier = '[Blocked]'},
+    IdentifyPremium = {Toggle = true, Identifier = '[Premium]'},
+    IdentifyDeveloper = {Toggle = true, Identifier = '[Game Dev]'},
+    SpoofLocalPlayer = {Toggle = false, UseRandomName = true, NewName = 'Random Name Lol'},
+    Orientation = 'Vertical'
+}
+wait()
+  end
