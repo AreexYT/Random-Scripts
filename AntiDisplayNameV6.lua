63eae94a7dc71d0eebcbefb32748beaa63e4ba62
Searching4Myeslf <88655156+AreexYT@users.noreply.github.com>
2022-06-01 21:03:58 -0700

    Update AntiDisplayNameV6.lua

--- AntiDisplayNameV6.lua
+++ AntiDisplayNameV6.lua
@@ -26,8 +26,8 @@ task.spawn(function()
 
     local LP, Players, ValidStatuses, PlayerInfoPrefetch = game:GetService('Players').LocalPlayer, game:GetService('Players'), {Enum.FriendStatus.NotFriend, Enum.FriendStatus.Friend}, {
         Friend = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '228, 494'},
-        Blocked = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '494, 94'},
-        Premium = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '132, 494'},
+        Blocked = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '494, 276'},
+        Premium = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '108, 492'},
         Developer = {Image = 'rbxasset://LuaPackages/Packages/_Index/UIBlox/UIBlox/App/ImageSet/ImageAtlas/img_set_1x_4.png', Offset = '492, 476'}
     }
 
